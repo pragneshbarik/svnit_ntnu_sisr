4328a38a02d454face880b5afbe0b05bee5f83f2
hiren-2911 <u19ec145@eced.svnit.ac.in>
2023-03-25 15:42:52 +0530

    changed weights

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir /home/thor/projects/data/NTIRE2022_Challenge --save_dir /home/thor/projects/data/NTIRE2023_Challenge/results --model_id 0
-
+CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir [path to your data dir] --save_dir [path to your save dir] --model_id 06
